8bd33b147ce58c720d5edb2a2161a16ec7b99adc
B0OGI3 <iandavis1504@gmail.com>
2025-12-22 13:42:33 -0500

    Added parser and responses from genai

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from boogbot!")
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if api_key == None:
+    raise RuntimeError("Invalid API Key")
 
 
-if __name__ == "__main__":
-    main()
+client = genai.Client(api_key=api_key)
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
+response = client.models.generate_content(
+    model='gemini-2.5-flash', contents=args.user_prompt)
+
+if response.usage_metadata == None:
+    raise RuntimeError("No response metadata")
+
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+print(response.text)
+
+ 
