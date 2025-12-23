0b33fadf4d45401350880a3c55040eedd4a1cd1d
B0OGI3 <iandavis1504@gmail.com>
2025-12-22 20:55:29 -0500

    Added feedback loop

--- main.py
+++ main.py
@@ -19,38 +19,56 @@ parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
+
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-response = client.models.generate_content(
-    model='gemini-2.5-flash',
-    contents=messages,
-    config=types.GenerateContentConfig(
-        tools=[available_functions], system_instruction=system_prompt
-    ),
-)
+for i in range(20):
 
-if response.usage_metadata == None:
-    raise RuntimeError("No response metadata")
+    try:
+        response = client.models.generate_content(
+            model='gemini-2.5-flash',
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions], system_instruction=system_prompt
+            ),
+        )
 
-if args.verbose:
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        for candidate in response.candidates:
+            messages.append(candidate.content)
 
-if response.function_calls:
-    function_responses = []
-
-    for call in response.function_calls:
-        function_call_result = call_function(call, verbose=args.verbose)
-        part = function_call_result.parts[0]
-        if not getattr(part, "function_response", None):
-            raise RuntimeError("Function call returned no function_response")
-
-        function_responses.append(part)
+        if response.usage_metadata == None:
+            raise RuntimeError("No response metadata")
 
         if args.verbose:
-            print(f"-> {part.function_response.response}")
-else:
-    print(response.text)
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+        if response.function_calls:
+            function_responses = []
+
+            for call in response.function_calls:
+                function_call_result = call_function(call, verbose=args.verbose)
+                part = function_call_result.parts[0]
+                if not getattr(part, "function_response", None):
+                    raise RuntimeError("Function call returned no function_response")
+
+                function_responses.append(part)
+
+                if args.verbose:
+                    print(f"-> {part.function_response.response}")
+                
+            messages.append(types.Content(role="user", parts=function_responses))
+
+        elif not response.function_calls and response.text:
+            print(response.text)
+            break
+        else:
+            print(response.text)
+        
+
+
+    except Exception as error:
+        print(f"Error: {error}")
 
  
